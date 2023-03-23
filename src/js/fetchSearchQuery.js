7df7a2817ec472267b2403bf732628c9db321ba5
Vitalii Khanaliiev <41639784+Despeir993@users.noreply.github.com>
2023-03-23 16:29:51 +0200

    f

--- src/js/fetchSearchQuery.js
+++ src/js/fetchSearchQuery.js
@@ -5,7 +5,7 @@ const API_KEY = "34526250-d4c92221e4f75204ff5bed4fd";
 const SEARCH_OPTIONS = "image_type=photo&orientation=horizontal&safesearch=true";
 export const LIMIT = 40;
     
-export async function fetchSearchQuery(searchQuery, pageNumber) {
+export async function fetchSrchQuery(searchQuery, pageNumber) {
     try {
         return await axios.get(`${baseUrl}?key=${API_KEY}&q=${searchQuery}&${SEARCH_OPTIONS}&page=${pageNumber}&per_page=${LIMIT}`);
     } catch (error) {
